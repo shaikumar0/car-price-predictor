9e37ee40f4fc99bcc3c0c570e344198599578651
shaikumar0 <23A91A0560@aec.edu.in>
2025-09-30 16:44:42 +0530

    Update server.py

--- car_pred/server.py
+++ car_pred/server.py
@@ -57,5 +57,9 @@ def submit():
     except Exception as e:
         return "internal server error here"
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True,port=3000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
